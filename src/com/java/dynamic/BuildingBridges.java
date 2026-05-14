a99ca45a4544f6b46fdb180ef0bbd6cf95ec4884
in46177609 <vivek.kumar2@tesco.com>
2026-05-15 00:52:28 +0530

    Target sum

--- src/com/java/dynamic/BuildingBridges.java
+++ src/com/java/dynamic/BuildingBridges.java
@@ -1,9 +1,9 @@
 package com.java.dynamic;
 
-import javafx.util.Pair;
-
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BuildingBridges {
     // Select max bridges without crossOver
@@ -11,20 +11,20 @@ public class BuildingBridges {
     // Find LIS of 2nd value
 
     public static void main(String[] args) {
-        List<Pair<Integer, Integer>> bridges1 = new ArrayList<>();
-        bridges1.add(new Pair<>(6, 2));  bridges1.add(new Pair<>(4, 3));
-        bridges1.add(new Pair<>(2, 6));  bridges1.add(new Pair<>(1, 5));
+        List<Map.Entry<Integer, Integer>> bridges1 = new ArrayList<>();
+        bridges1.add(new AbstractMap.SimpleEntry<>(6, 2));  bridges1.add(new AbstractMap.SimpleEntry<>(4, 3));
+        bridges1.add(new AbstractMap.SimpleEntry<>(2, 6));  bridges1.add(new AbstractMap.SimpleEntry<>(1, 5));
 
-        List<Pair<Integer, Integer>> bridges2 = new ArrayList<>();
-        bridges2.add(new Pair<>(8, 1)); bridges2.add(new Pair<>(1, 2));
-        bridges2.add(new Pair<>(4, 3)); bridges2.add(new Pair<>(3, 4));
-        bridges2.add(new Pair<>(2, 6)); bridges2.add(new Pair<>(6, 7));
-        bridges2.add(new Pair<>(7, 8)); bridges2.add(new Pair<>(5, 5));
+        List<Map.Entry<Integer, Integer>> bridges2 = new ArrayList<>();
+        bridges2.add(new AbstractMap.SimpleEntry<>(8, 1)); bridges2.add(new AbstractMap.SimpleEntry<>(1, 2));
+        bridges2.add(new AbstractMap.SimpleEntry<>(4, 3)); bridges2.add(new AbstractMap.SimpleEntry<>(3, 4));
+        bridges2.add(new AbstractMap.SimpleEntry<>(2, 6)); bridges2.add(new AbstractMap.SimpleEntry<>(6, 7));
+        bridges2.add(new AbstractMap.SimpleEntry<>(7, 8)); bridges2.add(new AbstractMap.SimpleEntry<>(5, 5));
 
-        List<Pair<Integer, Integer>> bridges3 = new ArrayList<>();
-        bridges3.add(new Pair<>(6, 2));  bridges3.add(new Pair<>(4, 3));
-        bridges3.add(new Pair<>(2, 6));  bridges3.add(new Pair<>(1, 5));
-        bridges3.add(new Pair<>(1, 3));
+        List<Map.Entry<Integer, Integer>> bridges3 = new ArrayList<>();
+        bridges3.add(new AbstractMap.SimpleEntry<>(6, 2));  bridges3.add(new AbstractMap.SimpleEntry<>(4, 3));
+        bridges3.add(new AbstractMap.SimpleEntry<>(2, 6));  bridges3.add(new AbstractMap.SimpleEntry<>(1, 5));
+        bridges3.add(new AbstractMap.SimpleEntry<>(1, 3));
 
         System.out.println(getBridges(bridges1));
         System.out.println(getBridges(bridges2));
@@ -32,7 +32,7 @@ public class BuildingBridges {
     }
 
 
-    private  static List<Pair<Integer, Integer>> getBridges(List<Pair<Integer, Integer>> bridges){
+    private  static List<Map.Entry<Integer, Integer>> getBridges(List<Map.Entry<Integer, Integer>> bridges){
         bridges.sort((o1, o2) -> {
             if (o1.getKey() > o2.getKey()) {
                 return 1;
@@ -44,14 +44,14 @@ public class BuildingBridges {
         });
 
         int[] lis = new int[bridges.size()];
-        List<List<Pair<Integer, Integer>>> res = new ArrayList<>();
-        List<Pair<Integer, Integer>> p = new ArrayList<>();
+        List<List<Map.Entry<Integer, Integer>>> res = new ArrayList<>();
+        List<Map.Entry<Integer, Integer>> p = new ArrayList<>();
         p.add(bridges.get(0));
         res.add(p);
         lis[0] = 1;
         for(int i=1; i<bridges.size(); i++){
             lis[i] = 1;
-            List<Pair<Integer, Integer>> q = new ArrayList<>();
+            List<Map.Entry<Integer, Integer>> q = new ArrayList<>();
             q.add(bridges.get(i));
             res.add(q);
             for(int j=0; j<i; j++){
